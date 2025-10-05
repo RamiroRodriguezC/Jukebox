954b4ab9842c91283466e086a42f9fcdf1f2f23c
Lautaro Battista <lautarobattista@hotmail.com>
2025-10-05 02:26:39 -0300

    getReviewsByCancionId y getById

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -4,5 +4,7 @@ const router = app.Router();
 const reviewController = require('../controllers/reviewController')
 
 router.get("/", reviewController.getAll);
+router.get("/:id", reviewController.getById);
+router.get("/cancion/:id", reviewController.getReviewsByCancionId);
 
 module.exports = router;
--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -5,6 +5,18 @@ async function getAllReviews() {
     return reviews;
 }
 
+async function getReviewById(id) {
+  const reviews = await Review.findById(id);
+  return reviews;
+}
+
+async function getReviewsByCancionId(idCancion) {
+  const reviews = await Review.find({ "cancion._id": idCancion, isDeleted: false });
+  return reviews;
+}
+
 module.exports = {
     getAllReviews,
+    getReviewById,
+    getReviewsByCancionId,
 };
