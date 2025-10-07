07593e766e587eed49656bdba3889f8c62c467ce
RamiroRodriguezC <ramirorodcas1112@gmail.com>
2025-10-07 11:12:50 -0300

    Create + delete in review

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -4,5 +4,11 @@ const router = app.Router();
 const reviewController = require('../controllers/reviewController')
 
 router.get("/", reviewController.getAll);
-router.get("/:id", reviewController.getById);
+router.post("/create",reviewController.createReview);  
+router.put("/:id",reviewController.updateReview);  // QUE EL UPDATE SE HAGA SOBRE EL MISMO USUARIO QUE ESTA PIDIENDO ESE UPDATE O QUE LO HAGA UN ADMIN
+router.delete("/:id",reviewController.deleteReview);  // QUE EL DELETE SE HAGA SOBRE EL MISMO USUARIO QUE ESTA PIDIENDO ESE UPDATE O QUE LO HAGA UN ADMIN
+router.get("/:id", reviewController.getById); 
+
+
+
 module.exports = router;
